0bf4ed59963027ef036e40b76cc6020e96cacb0d
Stříbrný Jiří <jiri.stribrny@seyfor.com>
2026-05-03 10:44:44 +0200

    Revert "added Contains to the settings service"
    This reverts commit ad18e50b5b43f4b467d56b1eab4ecf6ae47fd84d.

--- src/Pillaro.Dataverse.PluginFramework/Settings/SettingsService.cs
+++ src/Pillaro.Dataverse.PluginFramework/Settings/SettingsService.cs
@@ -110,11 +110,6 @@ public class SettingsService
         return val.Value;
     }
 
-    public bool Contains(string key)
-    {
-        return Contains(_organizationService, key);
-    }
-
     protected static string GetTextValue(IOrganizationService service, string key)
     {
         var entity = GetEntityByKey(service, key);
@@ -161,11 +156,6 @@ public class SettingsService
     {
         var i = GetValue<DateTime>(service, key);
         return i;
-    }
-
-    protected static bool Contains(IOrganizationService service, string key)
-    {
-        return GetEntityByKey(service, key) != null;
     }
 
     protected static Entity GetEntityByKey(IOrganizationService service, string key)
