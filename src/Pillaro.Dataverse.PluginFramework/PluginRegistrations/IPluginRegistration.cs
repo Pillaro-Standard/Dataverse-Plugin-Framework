1b699cac37ebffc2d1b852e09f42cee3db829f1b
Ján Mucha <jan.mucha@pillaro.cz>
2026-05-07 07:56:46 +0200

    Add plugin registration builder contracts

---
+++ src/Pillaro.Dataverse.PluginFramework/PluginRegistrations/IPluginRegistration.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xrm.Sdk;
+
+namespace Pillaro.Dataverse.PluginFramework.PluginRegistrations;
+
+public interface IPluginRegistration
+{
+    IPluginStepStageBuilder OnCreate<TEntity>(string stepId)
+        where TEntity : Entity;
+
+    IPluginUpdateStepStageBuilder OnUpdate<TEntity>(string stepId)
+        where TEntity : Entity;
+
+    IPluginStepStageBuilder OnDelete<TEntity>(string stepId)
+        where TEntity : Entity;
+
+    IPluginStepStageBuilder OnMessage(string stepId, string messageName);
+
+    IPluginStepStageBuilder OnMessage<TEntity>(string stepId, string messageName)
+        where TEntity : Entity;
+}
+
+public interface IPluginStepStageBuilder
+{
+    IPluginStepModeBuilder PreValidation();
+
+    IPluginStepModeBuilder PreOperation();
+
+    IPluginStepModeBuilder PostOperation();
+}
+
+public interface IPluginUpdateStepStageBuilder : IPluginStepStageBuilder
+{
+}
+
+public interface IPluginStepModeBuilder
+{
+    IPluginStepBuilder Synchronous();
+
+    IPluginStepBuilder Asynchronous();
+}
+
+public interface IPluginStepBuilder
+{
+    IPluginStepBuilder Rank(int rank);
+
+    IPluginStepBuilder WithPreImage(string imageId, string name, params string[] attributes);
+
+    IPluginStepBuilder WithPostImage(string imageId, string name, params string[] attributes);
+
+    IPluginStepBuilder RequiresConfirmation(PluginRisk risk, string reason, PluginDeploymentScope scope = PluginDeploymentScope.All);
+}
+
+public interface IPluginUpdateStepBuilder : IPluginStepBuilder
+{
+    IPluginUpdateStepBuilder WhenChanged(params string[] attributes);
+}
