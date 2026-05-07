ceb184e50da35c3104366ccc36f7f521bbc84014
Ján Mucha <jan.mucha@pillaro.cz>
2026-05-07 07:58:36 +0200

    Add plugin registration discovery helper

---
+++ src/Pillaro.Dataverse.PluginFramework/PluginRegistrations/PluginRegistrationDiscovery.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Xrm.Sdk;
+using System.Reflection;
+
+namespace Pillaro.Dataverse.PluginFramework.PluginRegistrations;
+
+public static class PluginRegistrationDiscovery
+{
+    private const string RegisterMethodName = "Register";
+
+    public static PluginRegistrationDescriptor Discover<TPlugin>()
+        where TPlugin : IPlugin
+    {
+        return Discover(typeof(TPlugin));
+    }
+
+    public static PluginRegistrationDescriptor Discover(Type pluginType)
+    {
+        if (pluginType == null)
+        {
+            throw new ArgumentNullException(nameof(pluginType));
+        }
+
+        if (!typeof(IPlugin).IsAssignableFrom(pluginType))
+        {
+            throw new ArgumentException($"Type '{pluginType.FullName}' must implement '{nameof(IPlugin)}'.", nameof(pluginType));
+        }
+
+        var builderType = typeof(PluginRegistrationBuilder<>).MakeGenericType(pluginType);
+        var builder = (IPluginRegistration)Activator.CreateInstance(builderType)!;
+
+        var registerMethod = GetRegisterMethod(pluginType);
+        registerMethod.Invoke(null, [builder]);
+
+        return (PluginRegistrationDescriptor)builderType
+            .GetMethod(nameof(PluginRegistrationBuilder<IPlugin>.Build), BindingFlags.Instance | BindingFlags.Public)!
+            .Invoke(builder, null)!;
+    }
+
+    public static IReadOnlyCollection<PluginRegistrationDescriptor> DiscoverFromAssembly(Assembly assembly)
+    {
+        if (assembly == null)
+        {
+            throw new ArgumentNullException(nameof(assembly));
+        }
+
+        return assembly
+            .GetTypes()
+            .Where(type => !type.IsAbstract && typeof(IPlugin).IsAssignableFrom(type))
+            .Where(HasRegisterMethod)
+            .Select(Discover)
+            .ToArray();
+    }
+
+    private static MethodInfo GetRegisterMethod(Type pluginType)
+    {
+        var method = pluginType.GetMethod(RegisterMethodName, BindingFlags.Public | BindingFlags.Static);
+        if (method == null)
+        {
+            throw new InvalidOperationException($"Plugin type '{pluginType.FullName}' must define public static void {RegisterMethodName}({nameof(IPluginRegistration)} registration).");
+        }
+
+        var parameters = method.GetParameters();
+        if (method.ReturnType != typeof(void) || parameters.Length != 1 || parameters[0].ParameterType != typeof(IPluginRegistration))
+        {
+            throw new InvalidOperationException($"Plugin type '{pluginType.FullName}' has invalid registration method signature. Expected public static void {RegisterMethodName}({nameof(IPluginRegistration)} registration).");
+        }
+
+        return method;
+    }
+
+    private static bool HasRegisterMethod(Type pluginType)
+    {
+        var method = pluginType.GetMethod(RegisterMethodName, BindingFlags.Public | BindingFlags.Static);
+        if (method == null)
+        {
+            return false;
+        }
+
+        var parameters = method.GetParameters();
+        return method.ReturnType == typeof(void)
+            && parameters.Length == 1
+            && parameters[0].ParameterType == typeof(IPluginRegistration);
+    }
+}
