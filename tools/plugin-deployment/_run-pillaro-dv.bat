8cfb413cc3ec4e411dd249269bcd297dc50f8511
Ján Mucha <jan.mucha@pillaro.cz>
2026-05-07 08:26:00 +0200

    Fix plugin deployment batch CLI argument quoting

--- tools/plugin-deployment/_run-pillaro-dv.bat
+++ tools/plugin-deployment/_run-pillaro-dv.bat
@@ -10,15 +10,15 @@ if "%PILLARO_DV_CLI%"=="" set "PILLARO_DV_CLI=pillaro-dv"
 call "%SCRIPT_DIR%_load-profile.bat"
 if errorlevel 1 exit /b %errorlevel%
 
-set "PILLARO_DV_AUTH_ARGS=--environment "%PILLARO_DV_URL%" --auth-type "%PILLARO_DV_AUTH_TYPE%""
+if /I "%PILLARO_DV_AUTH_TYPE%"=="ConnectionString" (
+    "%PILLARO_DV_CLI%" %* --connection-string "%PILLARO_DV_CONNECTION_STRING%"
+    exit /b %errorlevel%
+)
 
 if /I "%PILLARO_DV_AUTH_TYPE%"=="ClientSecret" (
-    set "PILLARO_DV_AUTH_ARGS=%PILLARO_DV_AUTH_ARGS% --tenant-id "%PILLARO_DV_TENANT_ID%" --client-id "%PILLARO_DV_CLIENT_ID%" --client-secret "%PILLARO_DV_CLIENT_SECRET%""
+    "%PILLARO_DV_CLI%" %* --environment "%PILLARO_DV_URL%" --auth-type "%PILLARO_DV_AUTH_TYPE%" --tenant-id "%PILLARO_DV_TENANT_ID%" --client-id "%PILLARO_DV_CLIENT_ID%" --client-secret "%PILLARO_DV_CLIENT_SECRET%"
+    exit /b %errorlevel%
 )
 
-if /I "%PILLARO_DV_AUTH_TYPE%"=="ConnectionString" (
-    set "PILLARO_DV_AUTH_ARGS=--connection-string "%PILLARO_DV_CONNECTION_STRING%""
-)
-
-%PILLARO_DV_CLI% %* %PILLARO_DV_AUTH_ARGS%
+"%PILLARO_DV_CLI%" %* --environment "%PILLARO_DV_URL%" --auth-type "%PILLARO_DV_AUTH_TYPE%"
 exit /b %errorlevel%
