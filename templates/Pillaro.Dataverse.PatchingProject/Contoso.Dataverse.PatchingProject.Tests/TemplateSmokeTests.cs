fdfc48e55020aadc95c860011de8503046d315da
Ján Mucha <jan.mucha@pillaro.cz>
2026-05-15 09:58:14 +0200

    Add template smoke tests

---
+++ templates/Pillaro.Dataverse.PatchingProject/Contoso.Dataverse.PatchingProject.Tests/TemplateSmokeTests.cs
@@ -0,0 +1,10 @@
+﻿namespace Contoso.Dataverse.PatchingProject.Tests;
+
+public class TemplateSmokeTests
+{
+    [Fact]
+    public void Template_Should_Create_Test_Project()
+    {
+        Assert.True(true);
+    }
+}
